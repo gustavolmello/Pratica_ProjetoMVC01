ec392b291bbf918dbbd32b6f7c8da5a94b9860d3
gustavolmello <gugarj@gmail.com>
2021-12-21 12:20:34 -0300

    Primeira versao

---
+++ ProjetoAspNEtMVC.Presentation/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAspNetMVC01.Presentation
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. 
+        // Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //definir o padrão de navegação do projeto 
+            //(CONTROLLER/VIEW)
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. 
+        // Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //definir o caminho da página inicial do projeto
+                endpoints.MapControllerRoute(
+                  name: "default", //página inicial
+                  pattern: "{controller=Home}/{action=Index}" //caminho
+               );
+            });
+        }
+    }
+}
